815296226d90a493bf7d2588bc9eceb3f5d9a0a4
oduodg <werewildfire@gmail.com>
2023-06-01 17:19:56 +0900

    Chore: favicon 적용, logo image 추가

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,12 +1,17 @@
-import Head from 'next/head'
-import Image from 'next/image'
-import styles from '@/styles/Home.module.scss'
+import Head from "next/head";
+import Image from "next/image";
+import styles from "@/styles/Home.module.scss";
 
 export default function Home() {
   return (
     <>
+      <Head>
+        <title>How-much</title>
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+      <img src="/images/logo.svg" />
       <h1>How-much</h1>
       <p>이 꽃 얼마에요?</p>
     </>
-  )
+  );
 }
