89b02006eb486a526365bd604adf0bf4f15edadf
oduodg <werewildfire@gmail.com>
2023-06-03 22:28:32 +0900

    Design: 시세조회 페이지 구성

---
+++ pages/prices.tsx
@@ -0,0 +1,18 @@
+import styles from "@styles/Prices.module.scss";
+import SideBar from "components/common/SideBar";
+
+const Prices = () => {
+  return (
+    <>
+      <SideBar />
+      <div className={styles.body}>
+        <div className={styles.titleBox}>
+          <h2 className={styles.pageTitle}>시세 조회</h2>
+          <p className={styles.pageDescription}>날짜, 품목, 품종별로 양재동 화훼공판장 시세를 조회할 수 있습니다.</p>
+        </div>
+      </div>
+    </>
+  );
+};
+
+export default Prices;
