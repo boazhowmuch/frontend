31fa434ef2eefa0996e9afdcf2fd40cbb773b9eb
oduodg <werewildfire@gmail.com>
2023-06-11 18:11:25 +0900

    Design: Sidebar 메뉴 버튼 생성

--- components/SideBar/index.tsx
+++ components/SideBar/index.tsx
@@ -1,24 +1,39 @@
-import Image from "next/image";
+import Link from "next/link";
 import styles from "./styles.module.scss";
 import { Logo, MoneyGraphIcon } from "@images/index";
+
 const SideBar = () => {
   return (
     <div className={styles.sideBarContainer}>
+      <Link href="/" style={{ textDecoration: "none" }}>
       <div className={styles.logoBox}>
-        <Image src={Logo} alt="logo" width={35} />
-				<p className={styles.serivceName}>How-much</p>
+        <Logo width={35} height={35} />
+        <p className={styles.serivceName}>How-much</p>
       </div>
+      </Link>
 
-			<div className={styles.menuBar}>
-				<div className={styles.menuBox}>
-					<Image className={styles.menuIcon} src={MoneyGraphIcon} alt="" width={16}/>
-          <p>시세 조회</p>
-				</div>
-        <div className={styles.menuBox}>
-					<Image className={styles.menuIcon} src={MoneyGraphIcon} alt="" width={16}/>
-          <p>시세 조회</p>
-				</div>
-			</div>
+      <div className={styles.menuBar}>
+        <Link href="/prices" style={{ textDecoration: "none" }}>
+          <button className={styles.menuBox}>
+            <MoneyGraphIcon
+              className={styles.menuIcon}
+              width={21}
+              height={21}
+            />
+            시세 조회
+          </button>
+        </Link>
+        <Link href="/prices" style={{ textDecoration: "none" }}>
+          <button className={styles.menuBox}>
+            <MoneyGraphIcon
+              className={styles.menuIcon}
+              width={21}
+              height={21}
+            />
+            시세 조회
+          </button>
+        </Link>
+      </div>
     </div>
   );
 };
