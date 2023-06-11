964c5efe4c435db5790732b7da6a5be554be777f
oduodg <werewildfire@gmail.com>
2023-06-11 20:55:54 +0900

    Chore: MUI 설정

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,7 +1,13 @@
-import '../styles/globals.scss';
+import "@styles/globals.scss";
+import { ThemeProvider } from '@mui/material'
+import { theme } from '../theme'
 
-import type { AppProps } from 'next/app'
+import type { AppProps } from "next/app";
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <ThemeProvider theme={theme}>
+      <Component {...pageProps} />
+    </ThemeProvider>
+  );
 }
